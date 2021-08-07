6a34541b0f5df9d1b28e9ca6c91bf7c0f3178a9d
Davi G Silva <davigsilva98@hotmail.com>
2021-08-07 01:58:20 -0300

    Basic routes, controllers and views needed on the site statically

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\CategoryController;
+use App\Http\Controllers\Site\BlogController;
+use App\Http\Controllers\Site\ContactController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::namespace('Site')->group(function () {
+    Route::get('/', [HomeController::class, '__invoke']);
+
+    Route::get('products', [CategoryController::class, 'index']);
+    Route::get('products/{slug}', [CategoryController::class, 'show']);
+
+    Route::get('blog', [BlogController::class, '__invoke']);
+
+    Route::view('about', 'site.about.index');
+
+    Route::get('contact', [ContactController::class, 'index']);
+    Route::post('contact', [ContactController::class, 'form']);
 });
